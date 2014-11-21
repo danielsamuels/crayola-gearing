39cc11e4547aa4955c52a7ebdf14269aee25a331
Daniel Samuels <daniel.samuels1@gmail.com>
2014-11-21 00:50:27 +0000

    Add sentry.

--- web.py
+++ web.py
@@ -1,8 +1,14 @@
+
+from raven.contrib.flask import Sentry
+
 from run import Runner
 
-from flask import Flask, make_response, send_file
+from flask import Flask, send_file
 app = Flask(__name__)
 app.debug = True
+app.config['SENTRY_DSN'] = 'https://bbe540f965174d2da70ffacd097c59db:6345ee5cc988477489f817f54eed054e@app.getsentry.com/32482'
+sentry = Sentry(app)
+
 
 @app.route("/")
 def run():
